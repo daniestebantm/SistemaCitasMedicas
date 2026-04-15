dc17571e1ac5cd30b85706508473c634b33f61b4
Daniel Tamayo <dani.tamayo.m2007@gmail.com>
2026-04-15 09:08:32 -0700

    Atributos, constructores, getters, y setters

--- Paciente.java
+++ Paciente.java
@@ -1,3 +1,18 @@
 public class Paciente {
-    
+    private String nombre;
+    private int edad;
+
+    public Paciente(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
 }
